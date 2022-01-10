3c7b5db546e1a647e77f810d258fe559985abc27
pranayjoshi <pranayjoshi4466@gmail.com>
2022-01-10 19:35:21 +0530

    updated

---
+++ scripts/donate.js
@@ -0,0 +1,39 @@
+var options = {
+    "key": "rzp_live_o4IccI7XzfPv4c", // Enter the Key ID generated from the Dashboard
+    "amount": "200", // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+    "currency": "INR",
+    "name": "Acme Corp",
+    "description": "Beyond Welkin Donation",
+    "image": "IMG_20220103_220808.png",
+    "order_id": "order_Ii1wQ7dmnnor2J", //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
+    "handler": function (response){
+        alert(response.razorpay_payment_id);
+        alert(response.razorpay_order_id);
+        alert(response.razorpay_signature)
+    },
+    "prefill": {
+        "name": "Gaurav Kumar",
+        "email": "gaurav.kumar@example.com",
+        "contact": "9999999999"
+    },
+    "notes": {
+        "address": "Razorpay Corporate Office"
+    },
+    "theme": {
+        "color": "#3399cc"
+    }
+};
+var rzp1 = new Razorpay(options);
+rzp1.on('payment.failed', function (response){
+        alert(response.error.code);
+        alert(response.error.description);
+        alert(response.error.source);
+        alert(response.error.step);
+        alert(response.error.reason);
+        alert(response.error.metadata.order_id);
+        alert(response.error.metadata.payment_id);
+});
+document.getElementById('donate').onclick = function(e){
+    rzp1.open();
+    e.preventDefault();
+}
